b20d9bed5aea6a405cf502492c4f5a6c45f0dbba
avery <shadycanes0450@gmail.com>
2025-09-29 10:49:13 -0400

    minimal working flake

--- flake.nix
+++ flake.nix
@@ -31,36 +31,5 @@
         default = self.packages.${system}.toolkit;
       }
     );
-
-    # Optional convenience: run a shell with tools without installing
-    devShells = forAllSystems (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            openssh
-            coreutils gawk gnused findutils
-            curl rsync jq yq
-            iproute2 iptables nftables
-            nmap masscan
-            htop tmux ripgrep fd
-            socat netcat tcpdump
-            unzip zip xz
-          ];
-          shellHook = ''
-            echo "[fleet-kit] Dev shell ready. Try: which nmap"
-          '';
-        };
-      }
-    );
-
-    #`nix run github:avocado-avery/nix` works (drops you in shell)
-    apps = forAllSystems (system: {
-      default = {
-        type = "app";
-        program = "${self.devShells.${system}.default}/bin/bash";
-      };
-    });
-  };
 }
-
+  };
