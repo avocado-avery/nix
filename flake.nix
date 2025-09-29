e766097eaecad9f2a5b0a5c7ba0a196c483a9321
avery <shadycanes0450@gmail.com>
2025-09-29 10:55:48 -0400

    minimal working flake

--- flake.nix
+++ flake.nix
@@ -4,33 +4,34 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, ... }:
-  let
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-    forAllSystems = f:
-      builtins.listToAttrs (map (s: { name = s; value = f s; }) systems);
-  in {
-    packages = forAllSystems (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in {
-        # A single meta-package composed of common tools
-        toolkit = pkgs.buildEnv {
-          name = "fleet-toolkit";
-          paths = with pkgs; [
-            coreutils gawk gnused findutils
-            curl wget rsync
-            jq yq fastfetch
-            iproute2 iptables nftables
-            nmap masscan
-            htop tmux ripgrep fd
-            socat netcat tcpdump
-            unzip zip xz
-          ];
-        };
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems =
+        f: builtins.listToAttrs (map (s: { name = s; value = f s; }) systems);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          # A single meta-package composed of common tools
+          toolkit = pkgs.buildEnv {
+            name = "fleet-toolkit";
+            paths = with pkgs; [
+              coreutils gawk gnused findutils
+              curl wget rsync
+              jq yq fastfetch
+              iproute2 iptables nftables
+              nmap masscan
+              htop tmux ripgrep fd
+              socat netcat tcpdump
+              unzip zip xz
+            ];
+          };
 
-        default = self.packages.${system}.toolkit;
-      }
-    );
-  }
+          default = self.packages.${system}.toolkit;
+        }
+      );
+    };
 }
 
